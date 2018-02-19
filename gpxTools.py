997d84d12120450bf7d90b48ba07e082b2ca5367
MigoMueller <m.mueller@astro.rug.nl>
2018-02-19 10:30:47 +0100

    Comments

--- gpxTools.py
+++ gpxTools.py
@@ -9,6 +9,8 @@
 # * Develop tool to fill time gaps (think: in Tahuna coverage) with 'filler' file (think: Strava file or files)
 # * Automate download from Tahuna: in app, share link w/ owncloud.  On computer, curl link, grep output for "?uniqueid=" and "href".  Will output two lines containing track URL.  Extract.  "curl --compressed url" provides GPX.  How do I get the equivalent of --compressed in Python?
 # * Overload privacyZone and stravaUpload so as to work from files in memory (ASCII lists?) rather than on the disk.  Parm fileOutput=True or something.
+# * Make Strava upload a method (token in keyring)
+# * in merge / privacyZone: correct bounding box / trip length (Strava keeps correcting them)
 
 
 import matplotlib.pyplot as plt
