e4972cae2e030da33e0ad47ddde48ab2b6eaf39a
MigoMueller <m.mueller@astro.rug.nl>
2018-02-22 16:58:53 +0100

    Install script: check if environment could be activated

--- installInConda.bash
+++ installInConda.bash
@@ -5,15 +5,16 @@ conda create --name gpx python=3 astropy matplotlib ipykernel ipython lxml carto
 # lxml: speeds up gpxpy
 # fiona: Pythonic IO-lib for geo data formats including gpx, shapes only (time info is forgotten).
 source activate gpx
-# To do 2018/02/20: check if 'activate' worked, stop here if it didn't
-conda install -c conda-forge gpxpy -y
-# support GPX files
-conda install -c conda-forge geopy -y
-# retrieve coords of addresses
-pip install stravalib
-
-source activate root
-conda install nb_conda -y
-# for kernels to appear in Jupyter notebooks
-
-source activate gpx
+if [ "$?" -eq "0" ]
+then
+    conda install -c conda-forge gpxpy -y
+    # support GPX files
+    conda install -c conda-forge geopy -y
+    # retrieve coords of addresses
+    pip install stravalib
+    # Strava
+    source activate root
+    conda install nb_conda -y
+    # for kernels to appear in Jupyter notebooks
+    source activate gpx
+fi
