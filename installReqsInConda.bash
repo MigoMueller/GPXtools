7259160f2d1d63786277e688e410143d7041679e
MigoMueller <m.mueller@astro.rug.nl>
2019-10-28 07:02:43 +0100

    Updates since 2018

--- installReqsInConda.bash
+++ installReqsInConda.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
-source activate root
-conda create --name gpx python=3 astropy matplotlib ipykernel ipython lxml cartopy fiona -y
+conda activate root
+conda install nb_conda -y
+# for kernels to appear in Jupyter notebooks
+conda create --name gpx python=3 astropy matplotlib ipykernel jupyter ipython lxml cartopy fiona -y
 # ipykernel: so this env shows up in Jupyter notebook
 # lxml: speeds up gpxpy
 # fiona: Pythonic IO-lib for geo data formats including gpx, shapes only (time info is forgotten).
-source activate gpx
+conda activate gpx
 if [ "$?" -eq "0" ]
 then
     conda install -c conda-forge gpxpy -y
@@ -13,8 +15,4 @@ then
     # retrieve coords of addresses
     pip install stravalib
     # Strava
-    source activate root
-    conda install nb_conda -y
-    # for kernels to appear in Jupyter notebooks
-    source activate gpx
 fi
