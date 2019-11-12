cf3533a5bf0854ccf6ddba12251e8ad9d192d924
MigoMueller <m.mueller@astro.rug.nl>
2019-11-12 11:20:44 +0100

    StravaAuthenticator (issue #11): part 4 (simplifications)

--- GPXtools/stravaUploader.py
+++ GPXtools/stravaUploader.py
@@ -33,12 +33,15 @@ class stravaUploader():
     Update 2019/11/09+, M.Mueller@astro.rug.nl:
       new Strava authentication scheme using access tokens and refresh tokens.
     """
+    ### let these be defined in constructor:
     tokenFile='token' # access token; expiry date; refresh token
     clientIDfile = 'client.secret' # IDs my software: clientID,secret (separated by comma)
     scopesNeeded = ['activity:read_all','activity:write']  # hard-wire for now.  Make more flexible for stand-alone authenticator class?
     port = 5000
     redirectHost='localhost'
 
+    tokenDictKeys = ['access_token', 'expires_at', 'refresh_token']
+    
     def authenticateFromFile( self ):
         """
         Connect client using refresh token, return whether or not that succeeded
@@ -47,14 +50,13 @@ class stravaUploader():
             return False
         dummy = open(self.tokenFile).read().strip().split()
         assert len(dummy) == 3
-        dummy[1] = int(dummy[1])
+        dummy[1] = int(dummy[1]) # expires_at is int (seconds since epoch)
         self.client.access_token = dummy[0]
-        self.client.tokenDict = {}
-        keys = ['access_token', 'expires_at', 'refresh_token']
-        for k,v in zip(keys,dummy) :
-            self.client.tokenDict[k]=v
+        self.tokenDict = {}
+        for k,v in zip(self.tokenDictKeys,dummy) :
+            self.tokenDict[k]=v
         ### Check if access_token is valid for at least another hour
-        if self.client.tokenDict['expires_at'] - time.time() > 3600 : # time in seconds since epoch
+        if self.tokenDict['expires_at'] - time.time() > 3600 : # time in seconds since epoch
             ## future: if thoroughCheck do check
             print( 'access token should be good, still' )
             return True
@@ -77,7 +79,7 @@ class stravaUploader():
         """
         Retrieve new access token and save in client.  Save (new?) refresh token in file.
         """
-        refreshToken = self.client.tokenDict['refresh_token']
+        refreshToken = self.tokenDict['refresh_token']
         self.updateTokens( self.client.refresh_access_token( \
             client_id=self.cl_id, client_secret=self.cl_secret, \
             refresh_token=refreshToken ))
@@ -90,8 +92,8 @@ class stravaUploader():
         Argument response assumed to be dictionary with three elements 'access_token', 'refresh_token', 'expires_at'
         """
         #print( 'Updating tokens in client')
-        self.client.tokenDict = response
-        self.client.access_token = self.client.tokenDict['access_token']
+        self.tokenDict = response
+        self.client.access_token = self.tokenDict['access_token']
         athlete=self.client.get_athlete() # does access token work?
         outputText = "%s %i %s"%(response['access_token'], response['expires_at'], response['refresh_token'])
         open(self.tokenFile, 'w').write(outputText+'\n')
@@ -164,15 +166,25 @@ class stravaUploader():
         return
 
     
-    def __init__(self, inputFileName, activityName=None, commute=None, private=None, batchmode=False):
+    def __init__( self, inputFileName, activityName=None, commute=None, private=None, batchmode=False, checkAccessAlwaysThorough=False ):
         # batchmode: won't open web browser, neither to view track, nor to get user consent if no token is present
+        # checkAccessAlwaysThorough: when checking access, try to actually
+        #   connect to Strava.  If False (and not set in calls to method
+        #   ensureAccess), access_token is only checked locally,
+        #   against its expiry date.
+        #
+        # !!!!!!!!
+        # TO BE IMPLEMENTED 
+        # !!!!!!!!
+        #
+        self.checkAccessAlwaysThorough=checkAccessAlwaysThorough
         self.client=Client()
         self.batchmode = batchmode
         # Read in client ID and password:
         self.cl_id,self.cl_secret=open(self.clientIDfile).read().strip().split(',')
         if not self.authenticateFromFile():
             if batchmode:
-                print("No token present in batch mode: Strava upload failed")
+                print("No token present in batch mode: Strava authentication failed")
                 return
             try :
                 self.getUserConsent()
@@ -187,8 +199,8 @@ class stravaUploader():
         try:
             fileObject=open(inputFileName,'r')
         except:
-            # file couldn't be opened for reading
-            raise
+            print( "Input file %s couldn't be opened for reading"%inputFileName )
+            return
         try:
             returnValue=self.client.upload_activity(fileObject, data_type='gpx', activity_type='ride', private=True)
             print("Track uploaded to Strava, processing")
